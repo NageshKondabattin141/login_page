7c4aae648ac439734627845dd360a897dc7eee5f
NageshKondabattin141 <nageshkondabattin@gmail.com>
2024-01-04 21:27:37 +0530

    Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Login Page',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
